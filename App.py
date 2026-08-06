6ec2cabb9714193e5d5ba31f51512a4a27e82e4a
Moilovescats <alicemargaretjohnson@gmail.com>
2026-08-06 19:31:42 +0000

    I've got the buttonsss

--- App.py
+++ App.py
@@ -370,6 +370,9 @@ class AppHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "text/html; charset=utf-8")
         self.send_header("Content-Length", str(len(body)))
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
         self.end_headers()
         self.wfile.write(body)
 
